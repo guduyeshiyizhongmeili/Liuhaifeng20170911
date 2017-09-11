84af85686ae40611b1b5a4fecbbf92973f774113
liuhaifeng <1932736402@qq.com>
2017-09-11 14:03:17 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/test/liuhaifeng20170911/CartBean.java
@@ -0,0 +1,17 @@
+package com.bwie.test.liuhaifeng20170911;
+
+/**
+ * 刘海峰.10:12
+ */
+
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+
+
+}
+
